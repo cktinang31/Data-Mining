3144caec54caa118f80ba43cad4123331f625a53
kaneki-khen <144084528+kaneki-khen@users.noreply.github.com>
2024-07-11 07:59:21 -0400

    071224

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from linkedin_api import Linkedin
+from transformers import pipeline
+from transformers import AutoTokenizer
+import json
 
-api=Linkedin('kristineannecardosa32@gmail.com' , 'kristine031')
+api = Linkedin('kristineannecardosa32@gmail.com' , 'kristine031')
 
-profile=input("Input your linkedin username:")
+profile = input("Input your linkedin username:")
 
-print(api.get_profile(profile))
+profile_data = (api.get_profile(profile))
+
+print(json.dumps(profile_data, indent=2))
+
+# classifier = pipeline("")
+
+# res = classifier("", profile)
+
+# print(res)
---
+++ test.py
@@ -0,0 +1,9 @@
+from transformers import pipeline
+from transformers import AutoTokenizer
+
+
+classifier = pipeline("")
+
+res = classifier("")
+
+print(res)
